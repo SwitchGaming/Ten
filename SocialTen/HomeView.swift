b3df97d65e72289a249a4979fb89034ddc539a87
Sidharth <125322559+sidc43@users.noreply.github.com>
2025-12-14 22:31:31 -0600

    test

--- SocialTen/HomeView.swift
+++ SocialTen/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
     @State private var showRatingPicker = false
     @State private var showCreatePost = false
     @State private var startOnPromptTab = false
-// goat
+// goat 12
 // goat 2
     var body: some View {
         NavigationStack {
